8fceb89fedffb6baaede8ba8ebd50ae1fa7a3140
Jay <me@jirayu.in.th>
2026-04-02 04:05:39 +0700

    route guards

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,32 +1,44 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, UseInterceptors } from '@nestjs/common';
+import { AuthGuard } from "@nestjs/passport";
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { Roles } from "@src/auth/decorators/role.decorator";
+import { RolesGuard } from "@src/auth/guards/role.guard";
+import { JwtInterceptor } from "@src/auth/interceptors/jwt.interceptor";
+import { UserRole } from "@src/enum/userRole";
 
+@UseGuards(AuthGuard('jwt'), RolesGuard)
+@UseInterceptors(JwtInterceptor)
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
+  @Roles(UserRole.ADMIN, UserRole.USER)
   @Post()
   create(@Body() createOrderDto: CreateOrderDto) {
     return this.ordersService.create(createOrderDto);
   }
 
+  @Roles(UserRole.ADMIN, UserRole.USER)
   @Get()
   findAll() {
     return this.ordersService.findAll();
   }
 
+  @Roles(UserRole.ADMIN, UserRole.USER)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.ordersService.findOne(+id);
   }
 
+  @Roles(UserRole.ADMIN)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
     return this.ordersService.update(+id, updateOrderDto);
   }
 
+  @Roles(UserRole.ADMIN)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.ordersService.remove(+id);
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, UseInterceptors } from '@nestjs/common';
+import { AuthGuard } from "@nestjs/passport";
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Public } from "@src/auth/decorators/public.decorator";
+import { Roles } from "@src/auth/decorators/role.decorator";
+import { RolesGuard } from "@src/auth/guards/role.guard";
+import { JwtInterceptor } from "@src/auth/interceptors/jwt.interceptor";
+import { UserRole } from "@src/enum/userRole";
 
+@Public()
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles(UserRole.ADMIN)
+  @UseInterceptors(JwtInterceptor)
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
@@ -22,11 +32,17 @@ export class ProductsController {
     return this.productsService.findOne(+id);
   }
 
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles(UserRole.ADMIN)
+  @UseInterceptors(JwtInterceptor)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(+id, updateProductDto);
   }
 
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles(UserRole.ADMIN)
+  @UseInterceptors(JwtInterceptor)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.productsService.remove(+id);
