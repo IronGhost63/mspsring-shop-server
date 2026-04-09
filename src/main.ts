c6f8af1292063eb40301a247063e8b0f5245139d
Jay <me@jirayu.in.th>
2026-04-09 22:41:15 +0700

    serve uploaded file

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import cookieParser from "cookie-parser";
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.use(cookieParser());
+
+  app.useStaticAssets(
+    join(__dirname, '../..', 'uploads'),
+    {
+      prefix: '/public/uploads/',
+    }
+  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
